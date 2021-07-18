193861c19eae1d5e2cbedbb1c3c1c7e11bb65801
rahulpathak-github <62535734+rahulpathak-github@users.noreply.github.com>
2021-07-18 14:10:24 +0530

    Add socket to front/ no auth

--- client/src/components/canvas/Draw.js
+++ client/src/components/canvas/Draw.js
@@ -1,69 +1,96 @@
 import React, { useState, useEffect, useRef } from "react";
 import socketio from 'socket.io-client'
 
-const Draw = () => {
+const Draw = (props) => {
 
     const canvasRef = useRef(null);
-    const contextRef = useRef({});
+    const contextRef = useRef(null);
+    const drawContainerRef = useRef(null);
+    const timeoutRef = useRef()
     const [isDrawing, setIsDrawing] = useState(false);
-    const [color, setColor] = useState("black");
-
-    const prepareCanvas = () => {
-        const canvas = canvasRef.current;
-        canvas.width = window.innerWidth * 2;
-        canvas.height = window.innerHeight * 2;
-        canvas.style.width = `${window.innerWidth}px`;
-        canvas.style.height = `${window.innerHeight}px`;
-        const context = canvas.getContext("2d");
-        context.scale(2, 2);
-        context.lineCap = "round";
-        context.lineWidth = 4;
-        contextRef.current = context;
-    };
+    const mouse = useRef({ x: 0, y: 0 });
+    const last_mouse = useRef({ x: 0, y: 0 });
 
     useEffect(() => {
-        prepareCanvas();
-    }, []);
+        const canvas = canvasRef.current;
+        canvas.width = parseInt(getComputedStyle(drawContainerRef.current).getPropertyValue('width'));
+        canvas.height = parseInt(getComputedStyle(drawContainerRef.current).getPropertyValue('height'));
+        canvasRef.current.addEventListener('mousemove', function (e) {
+            last_mouse.current.x = mouse.current.x;
+            last_mouse.current.y = mouse.current.y;
 
-    const startDrawing = ({ nativeEvent }) => {
-        const { offsetX, offsetY } = nativeEvent;
+            mouse.current.x = e.pageX - this.offsetLeft;
+            mouse.current.y = e.pageY - this.offsetTop;
+        }, false);
+
+        const context = canvas.getContext("2d");
+        context.lineWidth = props.size;
+        context.lineJoin = 'round';
+        context.lineCap = 'round';
+        context.strokeStyle = props.color;
+        contextRef.current = context;
+    }, [])
+
+    useEffect(() => {
+        if (!props.imgFlag) { return }
+        var interval = setInterval(function () {
+            if (isDrawing) return;
+            setIsDrawing(true);
+            clearInterval(interval);
+            var image = new Image();
+            image.onload = function () {
+                contextRef.current.drawImage(image, 0, 0);
+                setIsDrawing(false);
+            };
+            image.src = props.base64ImageData;
+            props.setImgFlag(false);
+        }, 200)
+
+    }, [props.imgFlag])
+
+
+    useEffect(() => {
+        contextRef.current.lineWidth = props.size;
+        contextRef.current.strokeStyle = props.color;
+    }, [props.color, props.size])
+
+    const startDrawing = () => {
         contextRef.current.beginPath();
-        contextRef.current.moveTo(offsetX, offsetY);
+        contextRef.current.moveTo(last_mouse.current.x, last_mouse.current.y);
+        console.log(last_mouse.current.x, last_mouse.current.y);
         setIsDrawing(true);
-    };
+    }
 
     const finishDrawing = () => {
         contextRef.current.closePath();
         setIsDrawing(false);
-    };
+    }
 
-    const draw = ({ nativeEvent }) => {
+    const draw = () => {
         if (!isDrawing) {
-            return;
+            return
         }
-        const { offsetX, offsetY } = nativeEvent;
-        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.lineTo(mouse.current.x, mouse.current.y);
         contextRef.current.stroke();
-        contextRef.current.strokeStyle = color;
-    };
 
-    const clearCanvas = () => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext("2d");
-        context.fillStyle = "white";
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    };
+        if (timeoutRef.current != undefined) clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(function () {
+            console.log('img saved');
+            props.setBase64ImageData(canvasRef.current.toDataURL("image/png"));
+            props.sendImage();
+            clearTimeout(timeoutRef.current);
+        }, 1000)
+
+    }
 
     return (
-        <div>
-            <button onClick={clearCanvas}>Clear Canvas</button>
-            <canvas
-                id="canvas"
+        <div className="drawContainer" id="drawContainer" ref={drawContainerRef}>
+            <canvas className="draw" id="draw"
+                ref={canvasRef}
                 onMouseDown={startDrawing}
                 onMouseUp={finishDrawing}
                 onMouseMove={draw}
-                ref={canvasRef}
-            />
+            ></canvas>
         </div>
     );
 };
