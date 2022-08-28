40c065e98b21d813c528b91766cc51928ce582ed
anand-2020 <aa20@iitbbs.ac.in>
2022-08-28 14:56:57 +0530

    fixed navbar bug

--- client/src/components/UI/Navbar.js
+++ client/src/components/UI/Navbar.js
@@ -54,7 +54,7 @@ const Navbar = (props) => {
   const open = Boolean(anchorEl);
   const history = useHistory();
 
-  const { updateAuthData } = useContext(AuthContext);
+  const { authenticated, updateAuthData } = useContext(AuthContext);
   const { socket } = useContext(SocketContext);
   const [openCreateDialog, setOpenCreateDialog] = useState(false);
 
@@ -122,35 +122,37 @@ const Navbar = (props) => {
             </Fab>
           ) : null}
           <div className={classes.grow} />
-          <div>
-            <IconButton
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={open}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={goToProfile}>Profile</MenuItem>
-              <MenuItem onClick={handleLogout}>Logout</MenuItem>
-            </Menu>
-          </div>
+          {authenticated && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={goToProfile}>Profile</MenuItem>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </div>
